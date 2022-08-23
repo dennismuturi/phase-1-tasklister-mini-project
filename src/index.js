738166873d6a001504063f1d2e96a6151d849f2a
root <root@WIN-AVPLU2QPEMC.localdomain>
2022-08-24 00:14:52 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // Grab the form element
+ document.getElementById("create-task-form").addEventListener("submit",(event) =>{
+  event.preventDefault();
+  const description= document.querySelector("#new-task-description").value;
+    toDoList(description);
+  // console.log(event.timeStamp);
+ });
+
+   function toDoList(description){
+        let ul = document.querySelector("#tasks");
+        let li = document.createElement('li');
+        let btn = document.createElement("button");
+        btn.textContent= " x ";
+        btn.style.backgroundColor="black";
+        btn.style.color= "white";
+        btn.style.fontSize= "10px";
+        btn.addEventListener("click",(event) => {
+             event.target.parentNode.remove();
+        });
+        li.textContent = ` ${description} `;
+        li.style.listStyle="none";
+        li.appendChild(btn);
+        ul.appendChild(li);
+   }
 });
